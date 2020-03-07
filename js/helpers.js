3dccb3673d58bef8fb694ff08a7cbdda9f12c8e1
Anton Krivosheev <krivosheev777@gmail.com>
2020-03-07 18:20:02 +0300

    модули начало

---
+++ js/helpers.js
@@ -0,0 +1,22 @@
+'use strict';
+
+(function () {
+  window.helpers = {
+    getRandomNumber: function (min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    },
+    getRandomArrayElement: function (array) {
+      return array[this.getRandomNumber(0, array.length - 1)];
+    },
+    isEscEvent: function (evt, callback) {
+      if (evt.keyCode === window.constants.KEYS.ESC_KEYCODE) {
+        callback();
+      }
+    },
+    isEnterEvent: function (evt, callback) {
+      if (evt.keyCode === window.constants.KEYS.ENTER_KEYCODE) {
+        callback();
+      }
+    },
+  };
+})();
